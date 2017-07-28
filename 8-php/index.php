29ecb6bf5392844ee09053b0f23fb5b31ead839f
gabymq <gmorenogaby@gmail.com>
2017-07-27 21:03:13 -0500

    kh

---
+++ 8-php/index.php
@@ -0,0 +1,78 @@
+<?php
+    header("Content-type: text/html;charset=\"utf-8\"");
+
+    for ($i=1;$i<=10;$i++)
+        echo "<p>$i</p>";
+   
+
+
+
+
+
+
+
+/*$nombre = "Jose Luis";
+$nombre2 ="Curso de desarrollo web completo";
+echo "<html>\n\t<head>\n\t\t<meta charset=\"utf-8\">\n\t<head>";
+echo "\n<body>";
+echo "\n\t<p>Mi nombre es $nombre</p>";
+echo "\n\t<p>".$nombre." es el instructor del ".$nombre2."</p>";
+
+echo "\n</body";
+echo "\n</html>";
+$numero= 50;
+$calculo= $numero / 2 + 6;
+echo $calculo/4;
+
+$logico = true;
+echo "<p>El tipo booleando o logico siguiente tiene el valor de $logico</p>";
+$variableNombre = "nombre";
+echo $$variableNombre;
+
+Array
+$miArray = array(1,3,4,5,1);
+print_r($miArray);
+echo "<br><br>";
+echo "La longitud de mi array es ".sizeof($miArray);
+echo "<br><br>";
+$miArray2["dia"]="lunes";
+$miArray2["mes"]="agosto";
+$miArray2["year"]="2016";
+echo "<br><br>";
+var_dump($miArray2);
+unset($miArray2["year"]);
+echo "<br><br>";
+print_r($miArray2);
+$miArrayAlReves = array_reverse($miArray);
+echo "<br><br>";
+print_r($miArrayAlReves);
+$trozoArray = array_splice($miArray,2);
+echo "<br><br>";
+print_r($trozoArray);
+ if else
+  $usuario = "Jose Luis";
+    if ($usuario == "Rob" || $usuario == "Jose Luis")
+    {
+        //Instrucciones que se ejecutan si es cierto
+        echo "<h1>Bienvenido $usuario</h1>";
+    }
+    else
+    {
+        //En caso contrario
+        //vamos a mostrar lo siguiente
+        echo "<h1>Lo sentimos $usuario, no tiene acceso al sistema</h1>";
+    }
+    $edad=15;
+    //comprobar la mayoria de edad del usuario
+    if($edad >=18)
+    {
+        echo"<h1>Puede acceder al portal con esta edad</h1>";
+
+    }
+    else
+    {
+        echo "<h1>Lo sentimos tiene que tener al menos 18 años</h1>";
+    }
+
+*/
+?>
