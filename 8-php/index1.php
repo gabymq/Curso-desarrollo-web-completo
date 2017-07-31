21bd840fa68d3189c7a02a2d32ca2d539948204e
gabymq <gmorenogaby@gmail.com>
2017-07-31 17:21:51 -0500

    Formulario de contacto php practica 1 y 2

--- 8-php/index1.php
+++ 8-php/index1.php
@@ -1,3 +1,43 @@
+<?php
+    $error="";
+    $mensajeExito="";
+
+    if($_POST){
+        //comprobaciones sobre campos
+        if (!$_POST["email"]){
+            $error .= "Es obligatoria una direccion de email.<br>";
+        }
+        if (!$_POST["contenido"]){
+            $error .= "Es obligatorio escribir un mensaje.<br>";
+        }
+        if (!$_POST["asunto"]){
+            $error .= "Es obligatorio el campo asunto.<br>";
+        }
+        if ($_POST["email"] && filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)=== false){
+            $error .= "Correo electronico no valido.<br>";
+        }
+        if ($error !="")
+        {
+            $error = '<div class="alert alert-danger" role="alert"><p>Hubo algún error en el formulario:</p>'.$error."<strong></div>";
+        }
+        else
+        {
+            $emailA = "correoagmorenogaby@gmail.com";
+            $asunto = $_POST['asunto'];
+            $contenido = $_POST['contenido'];
+            $cabeceras = "From:".$_POST['email'];
+            if (mail($emailA,$asunto,$contenido,$cabeceras)){
+                $mensajeExito = '<div class="alert alert-success" role="alert">Mensaje enviado con éxito, nos pondremos en contacto pronto!</div>';
+            }
+            else{
+                $error =$error ='<div class="alert alert-danger" role="alert"><p><strong>El mensaje no pudo ser enviado, por favor inténtalo más tarde</div>';
+            }
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,51 +50,58 @@
   </head>
   <body>
     <div class="container">
+            <h1>Contact us!!!</h1>
+            <div id="error"><? echo $error.$mensajeExito; ?></div>
             <form method="post">
-                    <h1>Contact Us!!!</h1>
-                    <div id="error"></div>
-                    <fieldset class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="email" class="form-control" id="email"  placeholder="Enter email" name="email>
-                        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </fieldset>
-                    <fieldset class="form-group">
-                        <label for="asunto">Affair</label>
-                        <input type="text" class="form-control" id="asunto" placeholder="Affair" name="asunto">
-                    </fieldset>
-                
-                    <fieldset class="form-group">
-                        <label for="contenido">What is your question?</label>
-                        <textarea class="form-control" id="contenido" name="contenido" rows="3"></textarea>
-                    </fieldset>
+                   <fieldset class="form-group">
+                  <label for="email">Adress email</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+                  <small class="text-muted">We will not share your email with anyone.</small>
+            </fieldset>
+            <fieldset class="form-group">
+                  <label for="asunto">Affair</label>
+                  <input type="text" class="form-control" id="asunto" name="asunto" >
+            </fieldset>
+            <fieldset class="form-group">
+                  <label for="contenido">What is your question?</label>
+                  <textarea class="form-control" id="contenido" name="contenido" rows="3"></textarea>
+            </fieldset>
+                  <button type="submit" id="submit" class="btn btn-primary">Send</button>
             
             
-            <button type="submit" id="submit" class="btn btn-primary">Send</button>
             </form>
     </div>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
         <script type="text/javascript">
             $("form").submit(function(e){
-                var error="";
-                if ($("#email").val()==""){
-                    error += "Field email required.<br>";
+            
+
+                var error= "";
+
+                if ($("#email").val()=="")
+                {
+                    error += "email required.<br>";
                 }
-                if ($("#asunto").val()==""){
+                if ($("#asunto").val()=="")
+                {
                     error += "Field affair required.<br>";
                 }
-                if ($("#contenido").val()==""){
+                if ($("#contenido").val()=="")
+                {
                     error += "Field content required.<br>";
                 }
                 if (error !="")
                 {
-                    $("#error").html('<div class="alert alert-danger" role="alert">
-                    <strong>Error</strong><br>'+error+'
-                    </div>');
+                    $("#error").html('<div class="alert alert-danger" role="alert"><p><strong>Hubo algún error en el formulario</strong></p>' + error + '</div>');
+                    return false;
+                }
+                else
+                {
+                    return true;
                 }
             })
         
