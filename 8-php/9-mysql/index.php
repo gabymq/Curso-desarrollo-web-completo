38bcfa97fdc8f26a5cd6a43f0ffffdc6cdb1ddb1
gabymq <gmorenogaby@gmail.com>
2017-08-01 19:20:56 -0500

    Actilizar una fila!!!

--- 8-php/9-mysql/index.php
+++ 8-php/9-mysql/index.php
@@ -12,11 +12,21 @@ if (!$link) {
 else{
 // echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 // echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+// Anadir un usuario a nuestra base de datos
+
+    // $query ="INSERT INTO usuarios(username,password)VALUES('pepe','323232')";
+    // mysqli_query($link,$query);
+//Actualizar una fila Y TAMBIEN PUDEO USAR username en lugar de id para actualizar y poner lo correspondiente
+$query ="UPDATE usuarios SET password='comecome' WHERE id=3 LIMIT 1";
+    mysqli_query($link,$query); 
+
     $query = "SELECT * from usuarios";
     if ($result = mysqli_query($link,$query))
     {
         $fila = mysqli_fetch_array($result);
-        print_r($fila);
+        // print_r($fila);
+        echo "Tu nombre de usuario es ".$fila['username']."y tu password es ".$fila['password']; 
     }
     
 }
