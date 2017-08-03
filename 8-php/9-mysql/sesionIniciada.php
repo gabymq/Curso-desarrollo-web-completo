aa93fb980b652d919443196f269360471e80029f
gabymq <gmorenogaby@gmail.com>
2017-08-02 22:43:31 -0500

    sesion inciada

--- 8-php/9-mysql/sesionIniciada.php
+++ 8-php/9-mysql/sesionIniciada.php
@@ -2,7 +2,7 @@
     session_start();
     if (array_key_exists("id",$_COOKIE))
     {
-        $_SESSION['id'] = $_SESSION['id'];
+        $_SESSION['id'] = $_COOKIE['id'];
     }
     if (array_key_exists("id",$_SESSION))
     {
