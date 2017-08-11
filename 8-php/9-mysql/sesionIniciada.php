e31b2bb46777cbbf0f2cbbe62fce0cea63510b40
gabymq <gmorenogaby@gmail.com>
2017-08-11 14:05:40 -0500

    sesion iniciada

--- 8-php/9-mysql/sesionIniciada.php
+++ 8-php/9-mysql/sesionIniciada.php
@@ -1,15 +1,43 @@
 <?php
     session_start();
-    if (array_key_exists("id",$_COOKIE))
+    $contenidoDiario="";
+    if (array_key_exists("id",$_COOKIE)&& $_COOKIE['id'])
     {
         $_SESSION['id'] = $_COOKIE['id'];
     }
     if (array_key_exists("id",$_SESSION))
     {
-        echo "<p>Sesion iniciada. <a href='index.php? Logout=1'>Cerrar sesion</a></p>";
+        include "conecction.php";
+        $query = "SELECT diario FROM usuarios WHERE id='".mysqli_real_escape_string($link,$_SESSION['id'])."' LIMIT 1";
+        $result=mysqli_real_escape_string($link,$query);
+        $result=mysqli_query($link,$query);
+
+        $fila=mysqli_fetch_array($result);
+        $contenidoDiario= $fila['diario'];
     }
     else
     {
         header("Location: index.php");
     }
+    include"header.php";
 ?>
+
+
+    <nav class="navbar navbar-ligth bg-faded navbar-fixed-top">
+
+        <a class="navbar-brand" href="#">Diario Seceto</a>
+
+     <div class="pull-xs-right">
+      <a href="index.php?Logout=1">
+      <button class="btn btn-outline-success" type="submit">Cerrar Sesion</button></a>
+
+    </div>
+    </nav>
+    <div class="container-fluid" id="contenedorSesionIniciada">
+    <textarea id="diario" class="form-control">
+    
+    <?php $contenidoDiario; ?></textarea>
+
+    </div>
+
+<?php include"footer.php"; ?>
