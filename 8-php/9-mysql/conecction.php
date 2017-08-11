9c4ea253a66b3d8954c7d547f6ad5a96cd4c236d
gabymq <gmorenogaby@gmail.com>
2017-08-11 14:03:40 -0500

    coeccion con db

---
+++ 8-php/9-mysql/conecction.php
@@ -0,0 +1,8 @@
+<?php
+    $link = mysqli_connect("127.0.0.1", "root", "", "diario");
+            if (mysqli_connect_error())
+            {
+                die("Hubo un error en la conexion, intentelo mas tarde");
+            }
+
+?>
